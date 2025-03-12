95f834f2ec4d50f1757f8ac0f9fb02d572babeef
aniketsonkerr <aniketsonkerr@gmail.com>
2025-03-12 15:53:05 +0530

    adding footer

--- task2/vite-project/src/App.jsx
+++ task2/vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400">
         <Header />
         <Outlet />
+        <Footer />
       </div>
     </>
   );
